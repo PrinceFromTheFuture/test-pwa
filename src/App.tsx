242a5ba2919382a7847fec29f4b474f8e0249c64
Amir Waisblay <amirwais.projects@gmail.com>
2024-09-09 00:00:17 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -7,19 +7,27 @@ export default function App() {
   return (
     <div className=" fixed inset-0 bg-slate-100 flex justify-center items-center">
       <motion.div
-        whileHover={{ scale: 1.1 }}
-        whileTap={{ scale: 0.9 }}
+        whileHover={{ scale: 1.02, backgroundColor: "rgb(198, 208, 220)" }}
+        whileTap={{ scale: 0.98, backgroundColor: "rgb(198, 208, 220)" }}
         ref={itemRef}
         onClick={() => setIsOpen(!isOpen)}
-        className=" bg-indigo-500 text-white p-5 rounded-lg select-none cursor-pointer"
+        className=" bg-slate-300 text-white p-5 rounded-lg select-none cursor-pointer"
       >
         Expense tracher
       </motion.div>
       <AnimatePresence>
         {isOpen && (
           <motion.div
-            className="fixed bg-indigo-500 rounded-lg select-none cursor-pointer"
+            className="fixed  rounded-lg select-none cursor-pointer"
+            style={{ backgroundColor: "rgb(198, 208, 220)" }}
+            transition={{
+              type: "tween",
+              ease: "easeOut",
+              duration: 0.2,
+              mass: 0.01,
+            }}
             onClick={() => setIsOpen(!isOpen)}
+            layout
             initial={{
               top: itemRef.current!.getBoundingClientRect().top,
               left: itemRef.current!.getBoundingClientRect().left,
