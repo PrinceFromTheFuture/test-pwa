685ee6647db3693aad2c20f0b76c0a83168eba22
Amir Waisblay <amirwais.projects@gmail.com>
2024-09-12 18:24:53 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -5,21 +5,21 @@ export default function App() {
   const [isOpen, setIsOpen] = useState(false);
   const itemRef = useRef<HTMLDivElement>(null);
   return (
-    <div className=" fixed inset-0 bg-slate-100 flex justify-center items-center">
+    <div className=" fixed inset-0 bg-[#F8FBFD] flex justify-center items-center shadow-lg">
       <motion.div
         whileHover={{ scale: 1.02, backgroundColor: "rgb(198, 208, 220)" }}
-        whileTap={{ scale: 0.98, backgroundColor: "rgb(198, 208, 220)" }}
+        whileTap={{ scale: 0.98, backgroundColor: "#f1f5f9  " }}
         ref={itemRef}
         onClick={() => setIsOpen(!isOpen)}
-        className=" bg-slate-300 text-white p-5 rounded-lg select-none cursor-pointer"
+        className="  bg-slate-100 border-lime-100 text-white p-5 rounded-lg select-none cursor-pointer"
       >
         Expense tracher
       </motion.div>
       <AnimatePresence>
         {isOpen && (
           <motion.div
-            className="fixed  rounded-lg select-none cursor-pointer"
-            style={{ backgroundColor: "rgb(198, 208, 220)" }}
+            className="fixed  rounded-lg select-none cursor-pointer shadow-lg p-4"
+            style={{ backgroundColor: "#f1f5f9  " }}
             transition={{
               type: "tween",
               ease: "easeOut",
@@ -35,10 +35,10 @@ export default function App() {
               height: itemRef.current!.getBoundingClientRect().height,
             }}
             animate={{
-              top: 0,
-              left: 0,
-              right: 0,
-              bottom: 0,
+              top: 20,
+              left: 20,
+              right: 20,
+              bottom: 20,
               width: "100%",
               height: "100%",
             }}
